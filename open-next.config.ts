fd0fb057f05ede54f2e3598b808233473eef6c9a
naveedahmedlabs <naveedahmed.labs@gmail.com>
2026-08-26 18:53:44 +0500

    fix: add edgeExternals to open-next.config.ts for esbuild bundling

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,5 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({});
+export default defineCloudflareConfig({
+  edgeExternals: ["@next/env", "jose", "pg-cloudflare", "drizzle-kit"],
+});
